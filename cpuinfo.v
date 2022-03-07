58a1ce83c7b98ed0fe3dd4a3617969d84bdbb6f9
T Maharshi Sanand Yadav <63612136+methodist20122016@users.noreply.github.com>
2022-03-07 10:54:12 +0530

    Add files via upload

---
+++ cpuinfo.v
@@ -0,0 +1,28 @@
+[oracle@rdsdbsrvr81 ~]$ cat /proc/cpuinfo
+processor       : 0
+vendor_id       : AuthenticAMD
+cpu family      : 23
+model           : 24
+model name      : AMD Ryzen 5 3400G with Radeon Vega Graphics
+stepping        : 1
+microcode       : 0x8108109
+cpu MHz         : 3693.063
+cache size      : 512 KB
+physical id     : 0
+siblings        : 1
+core id         : 0
+cpu cores       : 1
+apicid          : 0
+initial apicid  : 0
+fpu             : yes
+fpu_exception   : yes
+cpuid level     : 13
+wp              : yes
+flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 syscall nx mmxext fxsr_opt pdpe1gb rdtscp lm constant_tsc rep_good nopl tsc_reliable nonstop_tsc aperfmperf eagerfpu pni pclmulqdq ssse3 fma cx16 sse4_1 sse4_2 x2apic movbe popcnt aes xsave avx f16c rdrand hypervisor lahf_lm extapic cr8_legacy abm sse4a misalignsse 3dnowprefetch osvw arat vmmcall fsgsbase bmi1 avx2 smep bmi2 rdseed adx smap xsaveopt xsavec xgetbv1
+bugs            : fxsave_leak sysret_ss_attrs
+bogomips        : 7386.12
+TLB size        : 2560 4K pages
+clflush size    : 64
+cache_alignment : 64
+address sizes   : 42 bits physical, 48 bits virtual
+power management:
